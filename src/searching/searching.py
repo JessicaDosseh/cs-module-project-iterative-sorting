609c836b772e00c21b071814e5ccfdedbae4ad22
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-10-03 02:09:45 -0400

    Implement linear_search()

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,13 @@
 def linear_search(arr, target):
-    # Your code here
+    # Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
+    # If x matches with an element, return the index.
+    # If x doesn’t match with any of elements, return -1
+    for index, value in enumerate(arr):
+        if value == target:
+            print(index)
+            return index
 
-
-    return -1   # not found
+    return -1   # value not found
 
 
 # Write an iterative implementation of Binary Search
@@ -10,5 +15,8 @@ def binary_search(arr, target):
 
     # Your code here
 
-
     return -1  # not found
+
+
+test_array = [12, 20, 8, 1, 6, 14, 29, 10, 2, 5]
+linear_search(test_array, 6)
