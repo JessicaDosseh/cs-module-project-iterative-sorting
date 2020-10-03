d21714a82bfaff5e4e60f8dde05b6ff104cd0676
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-10-03 02:34:26 -0400

    Implement binary_search()

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -4,7 +4,7 @@ def linear_search(arr, target):
     # If x doesn’t match with any of elements, return -1
     for index, value in enumerate(arr):
         if value == target:
-            print(index)
+            print(f'linear_search: {index}')
             return index
 
     return -1   # value not found
@@ -12,11 +12,29 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    # Compare x with the middle element.
+    # If x matches with middle element, we return the mid index.
+    # Else If x is greater than the mid element, then x can only lie in right half subarray after the mid element. So we recur for right half.
+    # Else (x is smaller) recur for the left half.
 
-    # Your code here
+    # declaration
+    left = 0
+    right = len(arr) - 1
 
-    return -1  # not found
+    while left <= right:
+        middle = (left + right) // 2  # integer division
+
+        if arr[middle] == target:  # Check if x is present at mid
+            print(f'binary_search: {middle}')
+            return middle
+        elif arr[middle] < target:  # If x is greater, ignore left half
+            left = middle + 1
+        else:
+            right = middle - 1  # If x is smaller, ignore right half
+
+    return -1  # value not found
 
 
 test_array = [12, 20, 8, 1, 6, 14, 29, 10, 2, 5]
 linear_search(test_array, 6)
+binary_search(test_array, 6)
